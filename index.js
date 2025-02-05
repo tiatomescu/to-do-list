3ced36872b6093d65548fd6f777c19329f0e7495
tiatomescu <tia.yt.tomescu@gmail.com>
2025-02-05 09:04:21 -0800

    Fixed issue where deleting item would untoggle strikethroughs.

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 /* Global Variables */
-let allItems = [];
 let userInput = '';
+  //Document queries
+let allItemsList = document.querySelector('#all-items-list');
+let inputBox = document.querySelector('#input-box');
+let addBtn = document.querySelector('#add-item-btn');
 
-/* Dynamically displays items from allItems array */
-const displayItems = () => {
-  let allItemsList = document.querySelector('#all-items-list');
-  let listContents = allItems.map((elm) => `<div id="${allItems.indexOf(elm)}"><button class="item">${elm}</button><button class='x'>x</button></div>`)
-  .reduce((acc, elm) => acc += elm, '');
-  allItemsList.innerHTML = listContents;
-};
 
-/* On button click, grabs input box value, adds to allItems array, and displays */
+/* On button click, grabs input box value and adds to allItemsList */
 const getInputValue = () => {
-  let inputBox = document.querySelector('#input-box');
-  let addBtn = document.querySelector('#add-item-btn');
 
   inputBox.addEventListener('input', (eventObj) => {
     userInput = eventObj.target.value;
   });
 
-  addBtn.addEventListener('click', () => {
-    allItems.push(userInput);
+  addBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    allItemsList.innerHTML += `<span id="${allItemsList.childElementCount}"><button class="item">${userInput}</button><button class='x'>x</button></span>`;
     inputBox.value = '';
-    displayItems();
   });
 };
 
@@ -30,8 +24,7 @@ getInputValue();
 
 /* Changes '.item' button class when clicked */
 const changeButtonStyle = () => {
-  let allItemList = document.querySelector('#all-items-list');
-  allItemList.addEventListener('click', (eventObj) => {
+  allItemsList.addEventListener('click', (eventObj) => {
     if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className !== 'x'){
       let newClass = eventObj.target.className === 'item' ? 'item-crossed' : 'item';
       eventObj.target.className = newClass;
@@ -41,31 +34,28 @@ const changeButtonStyle = () => {
 
 changeButtonStyle();
 
-/* Removes '.item' from allItems when corresponding '.x' button clicked, then re-displays content*/
+/* Removes '.item' from allItemsList when corresponding '.x' button clicked*/
 
 const removeItem = () => {
   let allItemList = document.querySelector('#all-items-list');
   allItemList.addEventListener('click', (eventObj) => {
     if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className == 'x'){
-      let itemIndex = eventObj.target.parentElement.id;
-      allItems.splice(itemIndex, 1);
-      displayItems();
+      allItemsList.removeChild(eventObj.target.parentNode);
     }
   });
 };
 
 removeItem();
 
-/* Adds allItems list to local storage prior to refreshing, and calls it back when reloaded! */
+/* Adds allItemsList to local storage prior to refreshing, and calls it back when reloaded! */
 
 addEventListener('beforeunload', () => {
-  localStorage.setItem('currentItems', allItems);
+  localStorage.setItem('currentItems', allItemsList.innerHTML);
 });
 
 if(localStorage.getItem('currentItems')){
   const storedItems = localStorage.getItem('currentItems');
-  if (allItems.length == 0){
-    allItems = storedItems.split(',');
-    displayItems();
+  if (allItemsList.childElementCount == 0){
+    allItemsList.innerHTML = storedItems;
   }
 }
